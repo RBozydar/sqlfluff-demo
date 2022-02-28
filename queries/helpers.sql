1433603d4cf34cabe31f6087bd1e29fc0c77b3b2
robert.wasilewski <robert.wasilewski@revolut.com>
2022-02-28 15:53:50 +0100

    fix macro

--- queries/helpers.sql
+++ queries/helpers.sql
@@ -1,4 +1,6 @@
-{% macro test_helper() -%}
-, test_value as test_column
-, test_value_2 AS test_col2
-{%- endmacro %}
+{% macro test_helper() %}
+
+test_value as test_column,
+test_value_2 AS test_col2,
+
+{% endmacro %}
